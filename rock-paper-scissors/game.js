c0828152dc0dd52297de2e820e7ff2d1bc460db7
tscp6 <1284127668@qq.com>
2025-10-12 17:13:17 +0800

    Add UI for human choice and results informations

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -1,71 +1,94 @@
+const buttons = document.querySelector(".choice-buttons");
+const roundNum = document.querySelector("#round");
+const scores = document.querySelector("#score-info");
+const choice = document.querySelector("#choices");
+const result = document.querySelector("#results");
+const finalInfo = document.querySelector("#final-result");
+
 let humanScore = 0;
 let computerScore = 0;
 let round = 0;
+let lastChoice;
 
 function getComputerChoice() {
   let choiceNum = Math.floor(Math.random() * 3) + 1;
+  if(choiceNum === lastChoice) getComputerChoice();
+  lastChoice = choiceNum;
   if (choiceNum === 1) {
-    return "Rock";
+    return "rock";
   } else if (choiceNum === 2) {
-    return "Paper";
+    return "paper";
   } else if (choiceNum === 3) {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-function getHumanChoice() {
-  let choice = prompt("What's your choice?");
-  return choice.toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
+  round++;
+  roundNum.textContent = "Round : " + round;
+  choice.textContent =
+    "Human choice:" + humanChoice + " Computer choice:" + computerChoice;
   switch (humanChoice) {
     case "rock": {
-      if (computerChoice === "rock") console.log("Dogfall.");
+      if (computerChoice === "rock") result.textContent="Dogfall.";
       else if (computerChoice === "paper") {
-        console.log("You lose, paper beats rock.");
+        result.textContent="You lose, paper beats rock.";
         computerScore++;
       } else if (computerChoice === "scissors") {
-        console.log("You win, rock beats scissors.");
+        result.textContent="You win, rock beats scissors.";
         humanScore++;
       }
       break;
     }
     case "paper": {
       if (computerChoice === "rock") {
-        console.log("You win, paper beats rock.");
+        result.textContent="You win, paper beats rock.";
         humanScore++;
-      } else if (computerChoice === "paper") console.log("Dogfall.");
+      } else if (computerChoice === "paper") result.textContent="Dogfall.";
       else if (computerChoice === "scissors") {
-        console.log("You lose, scissors beats paper.");
+        result.textContent="You lose, scissors beats paper.";
         computerScore++;
       }
       break;
     }
     case "scissors": {
       if (computerChoice === "rock") {
-        console.log("You lose, rock beats scissors.");
+        result.textContent="You lose, rock beats scissors.";
         computerScore++;
       } else if (computerChoice === "paper") {
-        console.log("You win, scissors beats paper.");
+        result.textContent="You win, scissors beats paper.";
         humanScore++;
-      } else if (computerChoice === "scissors") console.log("Dogfall.");
+      } else if (computerChoice === "scissors") result.textContent="Dogfall.";
       break;
     }
     default:
       break;
   }
-  console.log(
-    "Human choice:" + humanChoice + " Computer choice:" + computerChoice
-  );
+  scores.textContent =
+    "Scores: Human: " + humanScore + " Computer: " + computerScore;
+  if (computerScore === 5) {
+    finalInfo.textContent="Computer Win!";
+    cleanRound();
+  }
+  else if(humanScore === 5){
+    finalInfo.textContent="Human Win!";
+    cleanRound();
+  }
 }
 
-for (round = 0; round < 5; round++) {
-  console.log("Round " + (round + 1));
-  playRound(getHumanChoice(), getComputerChoice().toLowerCase());
-  console.log("Human score:" + humanScore + " Computer score:" + computerScore);
+function cleanRound(){
+  round=0;
+  humanScore=0;
+  computerScore=0;
+  finalInfo.textContent += "Round refresh after 3 seconds";
+  setTimeout(() => {
+    location.reload();
+  }, 3000);
 }
 
-if (computerScore < humanScore) console.log("You win!");
-else if (computerScore === humanScore) console.log("Dogfall");
-else console.log("You lose.");
+roundNum.textContent = "Round : " + round;
+scores.textContent =
+  "Scores: Human: " + humanScore + " Computer: " + computerScore;
+buttons.addEventListener("click", (button) => {
+  playRound(button.target.id, getComputerChoice());
+});
